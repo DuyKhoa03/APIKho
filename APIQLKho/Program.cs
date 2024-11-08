ccb4f05573be89d054f5535b855a3056312ab3f3
ThinkPad <ThinkPad@DESKTOP-QR70HSK>
2024-11-08 10:05:18 +0700

    sss

--- APIQLKho/Program.cs
+++ APIQLKho/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 var connectionString =
-builder.Configuration.GetConnectionString("DefaultConnection");s
+builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<QlkhohangContext>(options =>
  options.UseSqlServer(connectionString));
 
